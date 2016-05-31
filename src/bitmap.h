252a88e1a4b67f3461efe7bbe4eb032b8139f7c9
draviolo <dimitris.raviolos@openbet.com>
2016-05-31 15:20:11 +0300

    closing #4

--- src/bitmap.h
+++ src/bitmap.h
@@ -4,7 +4,7 @@
 #define BIT (8*sizeof(unsigned char))
 #define BITMAP_NOTFOUND -1
 
-bool bitmapGet   (unsigned char *, int);
+int  bitmapGet   (unsigned char *, int);
 void bitmapSet   (unsigned char *, int);
 void bitmapReset (unsigned char *, int);
 int  bitmapSearch(unsigned char *, int, int, int);
